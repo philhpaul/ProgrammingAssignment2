7e4de65d3911cc495f2b51509158946f631f0658
phpaul <phpaul2010@gmail.com>
2014-12-18 09:53:12 -0800

    course assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cache a matrix inversion operation
+## 
 
-## Write a short comment describing this function
+## create a list to set and get value of a matrix and to set and get value of its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL  # m is mat_invrs, reset to NULL on calling
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get<-function(){x} #retn value of O mat
+  setinverse<-function(inverse) {m<<-inverse} # store as sup_assigned on first access
+  getinverse<-function(){m} #retn cached mat to cachematrix() on subsequent access
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) #list internal funcs
 }
 
 
-## Write a short comment describing this function
+## compute matrix inverse and set in cache if not already done
+## else recover inverse from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {   #if inverse already, returned as cached 
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)  # if m NULL, compute inverse
+  x$setinverse(m)        # store computed value
+  m                      # return result
 }
